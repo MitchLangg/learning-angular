6ff4e26d3b6a3a55df89e29e4cf444c451fd499e
Mitchell Lang <mitchlang444@gmail.com>
2020-10-21 15:14:46 -0400

    All Steps Complete

--- assignment3/src/app/app.module.ts
+++ assignment3/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
-import { ContentListComponent } from './content-list/content-list.component';
+import {ContentListComponent, FilterClass} from './content-list/content-list.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ContentCardComponent,
-    ContentListComponent
-  ],
+    declarations: [
+        AppComponent,
+        ContentCardComponent,
+        ContentListComponent,
+        FilterClass
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- assignment3/src/app/content-list/content-list.component.ts
+++ assignment3/src/app/content-list/content-list.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {Content} from '../helper-files/content-interface';
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({name: 'filter'})
+export class FilterClass implements PipeTransform{
+  transform(content: Content[], type: string): any{
+    return content.filter(p => p.type.includes(type));
+  }
+}
 
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
   styleUrls: ['./content-list.component.scss']
 })
+
+
 export class ContentListComponent implements OnInit {
   displayId: number;
+  output = '';
+  originalTitle = '';
   public array = new Array <Content>();
   constructor() {
     this.array = [{
@@ -52,6 +64,14 @@ export class ContentListComponent implements OnInit {
         tags: ['Starbucks']
     }];
   }
+  public titleCheck(input: string): any {
+    if (this.array.filter(p => p.title.toLowerCase().includes(input.toLowerCase())).length !== 0) {
+      this.output = 'That an existing title!';
+    }else {
+      this.output = 'Not an existing title :(';
+    }
+  }
+
   public clickForId(item: Content): void {
     this.displayId = item.id;
     console.log(this.displayId);
